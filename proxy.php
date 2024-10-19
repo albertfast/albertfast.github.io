1c6f4e7e547b1e0abac2a2d9b8f5d6d8f3dbb508
ahmetsahiner <119397720+albertfast@users.noreply.github.com>
2024-10-19 09:19:07 -0700

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -6,6 +6,8 @@ $url = "https://wurfl.io/api/v1/json/device";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
 
 // Execute request
 $response = curl_exec($ch);
@@ -19,4 +21,13 @@ header('Access-Control-Allow-Headers: Content-Type');
 // Set content type to JSON and output the response
 header('Content-Type: application/json');
 echo $response;
+
+// Check if the response is valid
+if ($response === false) {
+    echo "cURL Error: " . curl_error($ch);
+} else {
+    // Output the raw response for debugginga
+    echo $response;
+}
+
 ?>
