b75baa328663dc1812693d3910f2a553da21bfe7
ahmetsahiner <119397720+albertfast@users.noreply.github.com>
2024-10-30 16:11:20 -0700

    Update my-3d-app.js

--- my-3d-app/my-3d-app.js
+++ my-3d-app/my-3d-app.js
@@ -3,32 +3,30 @@ import { Canvas, useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 
 function RotatingBox() {
-    const mesh = useRef();
+  const mesh = useRef();
 
-    // Animasyon döngüsü
-    useFrame(() => {
-        if (mesh.current) {  // Ekstra kontrol
-            mesh.current.rotation.x += 0.01;
-            mesh.current.rotation.y += 0.01;
-        }
-    });
+  // Animation loop
+  useFrame(() => {
+    if (mesh.current) {
+      mesh.current.rotation.x += 0.01;
+      mesh.current.rotation.y += 0.01;
+    }
+  });
 
-    return (
-        <mesh ref={mesh} position={[0, 0, 0]}>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={'orange'} />
-        </mesh>
-    );
+  return (
+    <mesh ref={mesh} position={[0, 0, 0]}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={'orange'} />
+    </mesh>
+  );
 }
 
-function App() {
-    return (
-        <Canvas style={{ height: '500px', background: '#202020' }}>
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
-            <RotatingBox />
-        </Canvas>
-    );
+export default function My3DApp() {
+  return (
+    <Canvas style={{ height: '500px', background: '#202020' }}>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <RotatingBox />
+    </Canvas>
+  );
 }
-
-export default App;
