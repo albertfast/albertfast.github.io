40cc1fb250a20bbf2491e7516f516ca13d8bd8e5
ahmetsahiner <119397720+albertfast@users.noreply.github.com>
2024-10-30 16:12:30 -0700

    Update page.tsx

--- my-3d-app/app/page.tsx
+++ my-3d-app/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import My3DApp from "../my-3d-app";
 
 export default function Home() {
   return (
@@ -12,6 +13,10 @@ export default function Home() {
           height={38}
           priority
         />
+
+         {/* Embed the 3D Component */}
+         <My3DApp />
+        
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
